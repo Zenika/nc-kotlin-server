69c43f30649711619094be371cbe2488a96f6536
nlepage <19571875+nlepage@users.noreply.github.com>
2018-01-14 23:27:36 +0100

    Add result service and resource

---
+++ server/src/main/kotlin/com/zenika/nckotlinserver/service/ResultService.kt
@@ -0,0 +1,15 @@
+package com.zenika.nckotlinserver.service
+
+import com.zenika.nckotlinserver.repository.PlayerResultRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class ResultService {
+    @Autowired
+    lateinit var repository: PlayerResultRepository
+
+    fun list() = repository.getAll()
+            .sortedBy { it.time }
+            .sortedByDescending { it.score }
+}
